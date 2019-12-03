6efb76190f657bccef4a0ad27e4c4ced7b51f1b1
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-12-03 16:09:22 +0200

    MP-1301: WIP - missing tests, bulk sync plugins

--- src/Spryker/Zed/ProductOfferStock/Persistence/ProductOfferStockRepositoryInterface.php
+++ src/Spryker/Zed/ProductOfferStock/Persistence/ProductOfferStockRepositoryInterface.php
@@ -5,22 +5,17 @@
  * Use of this software requires acceptance of the Spryker Marketplace License Agreement. See LICENSE file.
  */
 
-namespace Spryker\Zed\ProductOfferStock\Business;
+namespace Spryker\Zed\ProductOfferStock\Persistence;
 
 use Generated\Shared\Transfer\ProductOfferStockRequestTransfer;
 use Spryker\DecimalObject\Decimal;
 
-interface ProductOfferStockFacadeInterface
+interface ProductOfferStockRepositoryInterface
 {
     /**
-     * Specification:
-     * - Returns product offer stock for provided request criteria.
-     *
-     * @api
-     *
      * @param \Generated\Shared\Transfer\ProductOfferStockRequestTransfer $productOfferStockRequestTransfer
      *
      * @return \Spryker\DecimalObject\Decimal
      */
-    public function calculateProductOfferStockForRequest(ProductOfferStockRequestTransfer $productOfferStockRequestTransfer): Decimal;
+    public function getProductOfferStockForRequest(ProductOfferStockRequestTransfer $productOfferStockRequestTransfer): Decimal;
 }
